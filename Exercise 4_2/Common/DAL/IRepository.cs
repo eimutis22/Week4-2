c476cbd7a3f4e89e5d20df64de10fd86346eb520
Eimutisg <eimutis.genys@gmail.com>
2018-03-18 23:14:27 +0000

    DAL

---
+++ Exercise 4_2/Common/DAL/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.DAL
+{
+    interface IRepository<T> : IDisposable
+    {
+        IEnumerable<T> GetAllItems();
+        T GetItemById(int id);
+
+        void InsertItem(T item);
+        void UpdateItem(T item);
+        void DeleteItem(int id);
+
+        void Save();
+    }
+}
---
+++ Exercise 4_2/Common/DAL/UserRepositoryEF.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Model;
+using CoreAPI;
+
+namespace Common.DAL
+{
+    class UserRepositoryEF : IUserRepository
+    {
+        private CoreContext context;
+
+        public UserRepositoryEF(CoreContext context)
+        {
+            this.context = context;
+        }
+
+        public void DeleteItem(int id)
+        {
+            var user = context.Users.Find(id);
+            context.Users.Remove(user);
+        }
+
+        public void DeleteItem(User user)
+        {
+            context.Users.Remove(user);
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+        public IEnumerable<User> GetAllItems()
+        {
+            return context.Users.ToList();
+        }
+
+        public User GetItemById(int id)
+        {
+            return context.Users.Find(id);
+        }
+
+        public void InsertItem(User item)
+        {
+            context.Users.Add(item);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void UpdateItem(User item)
+        {
+            context.Entry(item).CurrentValues.SetValues(item); // Not sure if 100% correct..
+        }
+    }
+}
