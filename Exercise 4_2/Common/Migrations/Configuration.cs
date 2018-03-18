d1b4dbf77a488299cebd35b1356aefd042acff3a
Eimutisg <eimutis.genys@gmail.com>
2018-03-18 23:19:26 +0000

    Made Controller

--- Exercise 4_2/Common/Migrations/Configuration.cs
+++ Exercise 4_2/Common/Migrations/Configuration.cs
@@ -1,20 +1,21 @@
 namespace Common.Migrations
 {
     using Common.Model;
+    using CommonContext;
     using System;
     using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<CoreAPI.CoreContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<CoreContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(CoreAPI.CoreContext context)
+        protected override void Seed(CoreContext context)
         {
             List<User> users = new List<User>()
             {
