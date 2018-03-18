154994fff546f3ccfe90a64f999a97fd65411d48
Eimutisg <eimutis.genys@gmail.com>
2018-03-18 23:38:42 +0000

    Web API Config

--- Exercise 4_2/CoreAPI/App_Start/WebApiConfig.cs
+++ Exercise 4_2/CoreAPI/App_Start/WebApiConfig.cs
@@ -10,6 +10,8 @@ namespace CoreAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
 
             // Web API routes
             config.MapHttpAttributeRoutes();
